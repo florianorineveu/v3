16d5b96d290ce68facdf733590be98bc2250a2f4
Florian "Ori" Neveu <hello@fnev.eu>
2025-10-31 14:19:51 +0100

    Setup project, docker, and some tools for better code!

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,65 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__)
+    ->exclude('var')
+    ->exclude('vendor')
+    ->exclude('node_modules')
+    ->notPath('src/Kernel.php')
+    ->notPath('tests/object-manager.php')
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+        '@PSR12' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'list_syntax' => ['syntax' => 'short'],
+        'ordered_imports' => [
+            'sort_algorithm' => 'alpha',
+            'imports_order' => ['class', 'function', 'const'],
+        ],
+        'single_import_per_statement' => true,
+        'blank_line_between_import_groups' => true,
+        'no_unused_imports' => true,
+        'not_operator_with_successor_space' => true,
+        'trailing_comma_in_multiline' => [
+            'elements' => ['arrays', 'arguments', 'parameters'],
+        ],
+        'concat_space' => [
+            'spacing' => 'one',
+        ],
+        'unary_operator_spaces' => true,
+        'yoda_style' => true,
+        'binary_operator_spaces' => [
+            'default' => 'single_space',
+        ],
+        'return_type_declaration' => ['space_before' => 'none'],
+        'blank_line_before_statement' => [
+            'statements' => ['break', 'continue', 'declare', 'return', 'throw', 'try'],
+        ],
+        'single_line_throw' => false,
+        'class_attributes_separation' => [
+            'elements' => [
+                'method' => 'one',
+                'property' => 'one',
+            ],
+        ],
+        'visibility_required' => [
+            'elements' => ['property', 'method', 'const'],
+        ],
+        'native_function_invocation' => [
+            'include' => ['@compiler_optimized'],
+            'scope' => 'namespaced',
+        ],
+        'declare_strict_types' => true,
+        'phpdoc_scalar' => true,
+        'phpdoc_single_line_var_spacing' => true,
+        'phpdoc_var_without_name' => true,
+        'phpdoc_align' => ['align' => 'left'],
+        'phpdoc_order' => true,
+        'phpdoc_trim' => true,
+    ])
+    ->setFinder($finder)
+    ->setRiskyAllowed(true)
+;
---
+++ tests/object-manager.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+if (!isset($_SERVER['APP_ENV'])) {
+    (new Dotenv())->bootEnv(__DIR__ . '/../.env');
+}
+
+$env = $_SERVER['APP_ENV'] ?? 'test';
+$debug = (bool) ($_SERVER['APP_DEBUG'] ?? true);
+
+$kernel = new Kernel($env, $debug);
+$kernel->boot();
+
+return $kernel->getContainer()->get('doctrine')->getManager();
